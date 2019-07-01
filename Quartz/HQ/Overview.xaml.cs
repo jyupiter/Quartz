0f1da464827bdcc753e44eefb975c2ac28910ea2
D4rkstalker <173622F@mymail.nyp.edu.sg>
2019-07-01 15:52:45 +0800

    date format Broke

--- Quartz/HQ/Overview.xaml.cs
+++ Quartz/HQ/Overview.xaml.cs
@@ -47,10 +47,9 @@ namespace Quartz.HQ
 			MemValues = new ChartValues<MeasureModel>();
 			DiskValues = new ChartValues<MeasureModel>();
 			NetValues = new ChartValues<MeasureModel>();
-
+			//Debug.WriteLine(value);
 			//lets set how to display the X Labels
-			DateTimeFormatter = value => new DateTime((long)value).ToString("mm:ss");
-
+			//DateTimeFormatter = value => new DateTime((long)value).ToString("hh:mm:ss");
 			//AxisStep forces the distance between each separator in the X axis
 			AxisStep = TimeSpan.FromSeconds(1).Ticks;
 			//AxisUnit forces lets the axis know that we are plotting seconds
@@ -105,11 +104,10 @@ namespace Quartz.HQ
 			//4:disk
 			//5:network
 			new Thread(StartMonitering).Start();
-			new Thread(Init).Start();
 
 			DataContext = this;
 		}
-		public static void UpdateGraphs(int index, double value, double _trend)
+		public static void UpdateGraphs(int index, double value)
 		{
 			//Debug.WriteLine(index + "||" + value);
 			//SeriesCollection[index].Values.Add(value);
@@ -120,7 +118,7 @@ namespace Quartz.HQ
 					GpuValues.Add(new MeasureModel
 					{
 						DateTime = DateTime.Now,
-						Value = _trend
+						Value = value
 					});
 					if (GpuValues.Count > 150) GpuValues.RemoveAt(0);
 					break;
@@ -128,7 +126,7 @@ namespace Quartz.HQ
 					CpuValues.Add(new MeasureModel
 					{
 						DateTime = DateTime.Now,
-						Value = _trend
+						Value = value
 					});
 					if (CpuValues.Count > 150) CpuValues.RemoveAt(0);
 					break;
@@ -136,7 +134,7 @@ namespace Quartz.HQ
 					MemValues.Add(new MeasureModel
 					{
 						DateTime = DateTime.Now,
-						Value = _trend
+						Value = value
 					});
 					if (MemValues.Count > 150) MemValues.RemoveAt(0);
 					break;
@@ -144,7 +142,7 @@ namespace Quartz.HQ
 					DiskValues.Add(new MeasureModel
 					{
 						DateTime = DateTime.Now,
-						Value = _trend
+						Value = value
 					});
 					if (DiskValues.Count > 150) DiskValues.RemoveAt(0);
 					break;
@@ -152,7 +150,7 @@ namespace Quartz.HQ
 					NetValues.Add(new MeasureModel
 					{
 						DateTime = DateTime.Now,
-						Value = _trend
+						Value = value
 					});
 					if (NetValues.Count > 150) NetValues.RemoveAt(0);
 					break;
@@ -165,19 +163,6 @@ namespace Quartz.HQ
 
 			//lets only use the last 150 values
 			
-		}
-
-		public static void Init()
-		{
-			while (true)
-			{
-				new Thread(GetCpu).Start();
-				//new Thread(GetGpu).Start();
-				new Thread(GetMem).Start();
-				new Thread(GetDisk).Start();
-				//new Thread(GetNetwork).Start();
-				Thread.Sleep(waitTime);
-			}
 		}
 
 		public static void StartMonitering()
@@ -200,36 +185,13 @@ namespace Quartz.HQ
 				//Debug.WriteLine(GPUs[0].ThermalInformation.CurrentThermalLevel);
 				//Debug.WriteLine(GPUs[0].UsageInformation.GPU);
 				//Debug.WriteLine("<-----					----->\n");
-				_gpuTrend = gpu;
 				gpu = GPUs[0].UsageInformation.GPU.Percentage;
-				_gpuTrend = gpu - _gpuTrend;
-				UpdateGraphs(0,gpu,_gpuTrend);
+				UpdateGraphs(0,gpu);
 			}
 			catch (Exception e)
 			{
 				Debug.WriteLine("<---!	NVApi not found	!--->");
 			}
-		}
-
-
-		public static void GetCpu()
-		{
-			UpdateGraphs(1, cpu,_cpuTrend);
-		}
-
-		public static void GetMem()
-		{
-			UpdateGraphs(2, mem,_memTrend);
-		}
-
-		public static void GetDisk()
-		{
-			UpdateGraphs(3, disk,_diskTrend);
-		}
-
-		public static void GetNetwork()
-		{
-			//UpdateGraphs(4, disk, _netTrend);
 		}
 
 		private static void CpuThread()
@@ -239,10 +201,7 @@ namespace Quartz.HQ
 				PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
 				cpuCounter.NextValue();
 				System.Threading.Thread.Sleep(waitTime);
-				_cpuTrend = cpu;
-				cpu = cpuCounter.NextValue();
-				_cpuTrend = cpu - _cpuTrend;
-				Console.WriteLine("Processor Usage: " + cpu);
+				UpdateGraphs(1, cpu);
 			}
 		}
 		private static void MemThread()
@@ -251,11 +210,10 @@ namespace Quartz.HQ
 			{
 				PerformanceCounter ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
 				ramCounter.NextValue();
-				System.Threading.Thread.Sleep(waitTime);
-				_memTrend = mem;
+				System.Threading.Thread.Sleep(waitTime/2);
 				mem = ramCounter.NextValue();
-				_memTrend = mem - _memTrend;
-				Console.WriteLine("Memory Used: " + mem);
+				UpdateGraphs(2, mem);
+				//Console.WriteLine("Memory Used: " + mem);
 			}
 		}
 		private static void DiskThread()
@@ -264,11 +222,10 @@ namespace Quartz.HQ
 			{
 				PerformanceCounter diskCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
 				diskCounter.NextValue();
-				System.Threading.Thread.Sleep(waitTime);
-				_diskTrend = disk;
+				System.Threading.Thread.Sleep(waitTime/2);
 				disk = diskCounter.NextValue();
-				_diskTrend = disk - _diskTrend;
-				Console.WriteLine("Disk usage: " + disk);
+				UpdateGraphs(3, disk);
+				//Console.WriteLine("Disk usage: " + disk);
 			}
 		}
 		private static void NetThread()
@@ -277,11 +234,9 @@ namespace Quartz.HQ
 			{
 				PerformanceCounter netCounter = new PerformanceCounter("PhysicalDisk", "% Disk Time", "_Total");
 				netCounter.NextValue();
-				System.Threading.Thread.Sleep(waitTime);
-				_netTrend = net;
+				System.Threading.Thread.Sleep(waitTime/2);
 				net = netCounter.NextValue();
-				_netTrend = net - _netTrend;
-				Console.WriteLine("Disk usage: " + net);
+				//Console.WriteLine("Disk usage: " + net);
 			}
 		}
 		private static void SetAxisLimits(DateTime now)
@@ -323,13 +278,6 @@ namespace Quartz.HQ
 		public static float net;
 		public static float gpu;
 		public static int waitTime; //ms
-		private static double _axisMax;
-		private static double _axisMin;
-		private static double _cpuTrend;
-		private static double _memTrend;
-		private static double _diskTrend;
-		private static double _netTrend;
-		private static double _gpuTrend;
 		public static ChartValues<MeasureModel> GpuValues { get; set; }
 		public static ChartValues<MeasureModel> CpuValues { get; set; }
 		public static ChartValues<MeasureModel> MemValues { get; set; }
@@ -338,6 +286,6 @@ namespace Quartz.HQ
 		public Func<double, string> DateTimeFormatter { get; set; }
 		public static double AxisStep { get; set; }
 		public static double AxisUnit { get; set; }
-
+		public static double ticks;
 	}
 }
