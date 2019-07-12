b390e0809cc0ad4a54732dbcc899a4e8e2216d17
jyupiter <173098D@mymail.nyp.edu.sg>
2019-07-12 16:34:50 +0800

    :(

--- Quartz/AG/SearchLogs.xaml.cs
+++ Quartz/AG/SearchLogs.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,97 @@ namespace Quartz.AG
         public SearchLogs()
         {
             InitializeComponent();
+            IDG();
         }
+
+        #region SetUp
+
+        private void IDG()
+        {
+            DataGridTextColumn indx = new DataGridTextColumn();
+            DataGridTextColumn evnt = new DataGridTextColumn();
+            DataGridTextColumn date = new DataGridTextColumn();
+            DataGridTextColumn time = new DataGridTextColumn();
+            DataGridTextColumn desc = new DataGridTextColumn();
+            SearchResultDataGrid.Columns.Add(indx);
+            SearchResultDataGrid.Columns.Add(evnt);
+            SearchResultDataGrid.Columns.Add(date);
+            SearchResultDataGrid.Columns.Add(time);
+            SearchResultDataGrid.Columns.Add(desc);
+            indx.Header = "#";
+            evnt.Header = "Event";
+            date.Header = "Date";
+            time.Header = "Time";
+            desc.Header = "Description";
+            indx.Binding = new Binding("I");
+            evnt.Binding = new Binding("E");
+            date.Binding = new Binding("A");
+            time.Binding = new Binding("T");
+            desc.Binding = new Binding("D");
+        }
+
+        private struct Record
+        {
+            public int I { get; set; }
+            public string E { get; set; }
+            public string A { get; set; }
+            public string T { get; set; }
+            public string D { get; set; }
+        }
+
+        private void Display(int _I, string _E, string _A, string _T, string _D)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                SearchResultDataGrid.Items.Add(new Record
+                {
+                    I = _I,
+                    E = _E,
+                    A = _A,
+                    T = _T,
+                    D = _D
+                });
+            });
+        }
+
+        #endregion
+
+        #region SearchLogs
+
+        private void LoadFromLogs()
+        {
+            /*
+            string[] raw = File.ReadAllLines(P);
+            List<string> l = new List<string>();
+
+            foreach(string s in raw)
+                l.Add(Regex.Match(s, @"\[([^)]*)\]").Groups[1].Value);
+            try
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    TargetDirectory.Text = l[0];
+                    EnableFiltering.IsChecked = bool.Parse(l[1]);
+                    FilterExtensions.Text = l[2];
+                    FilterInclude.IsChecked = bool.Parse(l[3]);
+                    FilterExclude.IsChecked = !bool.Parse(l[3]);
+                    LoggingDirectory.Text = l[4];
+                    EnableLogs.IsChecked = bool.Parse(l[5]);
+                });
+            }
+            catch(Exception)
+            {
+                //
+            }
+            */
+        }
+
+        private void RetrieveFromLucene()
+        {
+
+        }
+
+        #endregion
 
         private void SearchLogsBtn_Click(object sender, RoutedEventArgs e)
         {
