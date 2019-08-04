c9726a7573c3ef5f73a7dad4677bf04c39d04c35
jyupiter <173098D@mymail.nyp.edu.sg>
2019-08-04 14:43:01 +0800

    shove

---
+++ Quartz/AG/FileSummary.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace Quartz.AG
+{
+    /// <summary>
+    /// Interaction logic for FileSummary.xaml
+    /// </summary>
+    public partial class FileSummary : Page
+    {
+        public FileSummary()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
